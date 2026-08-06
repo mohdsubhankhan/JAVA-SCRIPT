9144a19868c9299f61af89f3a78c489050c1fe8b
mohdsubhankhan <khanmohdsubhan876@gmail.com>
2026-08-06 20:48:10 +0530

    update code

--- 1st.js
+++ 1st.js
@@ -1,1 +1,91 @@
-console.log("mohd subhan khan");
+// console.log("mohd subhan khan");
+
+const accountId = "123456";
+let accountMail ="subhan@gmail.com";
+var accounPass = "subhan123@";
+accountCity = "delhi";
+
+accountMail = "subhankhan";
+accountPass ="132334";
+accountCity ="lucknow";
+
+let accountState;
+
+//accountId="121212"; // This will throw an error because accountId is a constant and cannot be reassigned.
+
+// console.log(accountId);
+// console.log(accountMail);
+// console.log(accountPass);
+// console.log(accountCity);
+
+
+// console.table ({
+//     accountId,
+//     accountMail,
+//     accountPass,
+//     accountCity,
+//     accountState
+// });
+
+
+//conversion
+
+let somenumber = 123;
+let stringNumber = String(somenumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+let isLoggedIn = "subahn";
+let booleanValue = Boolean(isLoggedIn);
+// console.log(booleanValue);
+// console.log(typeof booleanValue);
+
+//operations
+let num1 = 10;
+let num2 = 20;
+let sum = -num1
+// console.log(sum);
+
+// console.log(num1 + num2);
+
+// console.log (2+2);
+// console.log (2-30);
+// console.log (2*2);
+// console.log (2/3);
+// console.log (2%7);          
+// console.log (2**5); // 2 raised to the power of 5  
+
+
+                  //               conversion of string to number
+
+
+                  let score1 = "123abc";
+
+                //   console.log(score1);
+                //   console.log(typeof score1);
+
+                  let score2 = Number(score1);
+                //   console.log(score2);
+                //   console.log(typeof score2);
+
+                  let score3 = 33
+                  let score4 = String(score3);
+                //   console.log(score4);
+                //   console.log(typeof score4);
+
+                  let score5 = undefined;
+                       let score6 = Boolean(score5);
+                //   console.log(score6);
+                //   console.log(typeof score6);
+
+
+                 //             operations
+
+
+console.log("35" > 22);
+console.log(822 > "767");
+console.log(true > 343);
+
+console.log("2"===2);
+console.log(2===3);
+console.log(2===2);
