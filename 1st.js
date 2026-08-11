3d55e38257c07026be9ad945d41076a1561c9bd8
mohdsubhankhan <khanmohdsubhan876@gmail.com>
2026-08-11 13:10:12 +0530

    array & its method

--- 1st.js
+++ 1st.js
@@ -135,7 +135,7 @@ const score = 400;
 const balance = new Number(200);
 // console.log(balance);
 
-// console.log(balance.toString().length.toFixed(2));
+// console.log(balance.toString().length.toFixed(3));
 
 const otherNumber = 123.8966;
 
@@ -170,11 +170,118 @@ const max= 20;
 
 
 // ********************  DATES ***************
-let date = new Date(2026, 7, 10);
 
-console.log(date);
-console.log(typeof  date);
+let date = new Date(2026, 7, 11);
+
+// console.log(date);
+// console.log(typeof  date);
  
-let q=date.getFullYear
-console.log(q)
+let q =   date.getFullYear()
+// console.log(q)
+
+let f = date.getMonth()  // in java script 0 is a consider as a january
+// console.log(f)
+
+let i = date.getDay()
+// console.log(i)
+
+
+let k = date.setMonth(5);
+let o = date.setDate(20);
+let m = date.setHours(10);
+let h = date.setFullYear(2030);
+
+// console.table({
+//   k,o,m,h
+
+// // });
+
+let date1 = new Date("2026-01-01");
+let date2 = new Date("2026-12-01");
+
+// console.log(date1 < date2);
+  
+// ****************  ARRAY  ******************
+
+const myarr = ["subhan", "amaan", "areeb", "ahtesham", "anam", "ahill"]
+
+const myarr2 = [0, 1, 2, 3, 4, 5]
+
+// console.log(myarr)
+
+// ************************* ARRAY METHOD ********************
+
+// myarr.push("adnan")       // add a value in array in the last
+// myarr.push("alman")
+
+// console.log(myarr)
+
+
+// myarr.pop()               // remove a value in array in the last
+// console.log(myarr)
+
+myarr2.unshift(6)      // shift a value of 0th index of array (6)add front 
+// console.log(myarr2)
+
+myarr2.shift()          // remove the 0th index of array (6) is remove 
+// console.log(myarr2)
+
+// console.log(myarr2)
+
+// console.log(myarr2.includes(9))     // it is check that 9 is in array or not so it is answer true or false 
+// console.log(myarr2.indexOf(2))   // it is a show that in array 2 indoex who so it is print 
+
+
+
+const newarr = myarr2.join()   // join() convert array to a string (typeof)
+// console.log(newarr)
+// console.log(typeof newarr)   // it convert to a string
+
+
+
+// ********************** SLICE & SPLICE ****************
+
+// console.log("A", myarr)
+const myn1 = myarr.slice(0,4)   // in slice array is not manipulate 
+// console.log(myn1)
+// console.log("B", myarr)
+
+const myn2 = myarr.splice(1,4)   // in splice array real value is manipulate 
+// console.log(myn2)
+// console.log("C", myarr)
+
+const myarr3 = ["subhan", "amaan", "areeb", "ahtesham"]
+const myarr4 = ["saman", "saniya", "mantasha", "zainab" ]
+
+// myarr3.push(myarr4)    // pushh another array in that array asa unofficial way
+// console.log(myarr3)
+
+// console.log(myarr3[4][1])   // get the value from array 
+
+// const concatarray = myarr3.concat(myarr4)  // concat means add all array in a good form 
+// console.log(concatarray)
+
+// const spredarray = [...myarr3, ...myarr4]   // it is use for spreading array in official way 
+// console.log(spredarray)
+
+const arr5 = [1, 2, 3, [4, 5], 6, 7, [8, 9, [10, 11]]]
+// console.log(arr5.flat(Infinity))  // it use for all array in join one array
+
+
+console.log(Array.isArray("SUBHAN"))   // IT IS  check that given is array or something else
+
+console.log(Array.from("SUBHAN"))   // IT IS  use for make array to anaything else
+
+console.log(Array.from({name:"subhan"}))  // it is not convert to an array because this is an object in object we tell that keys and value : its written an empty array 
+ 
+let score7 = 100
+let score8 = 200
+let score9 = 300
+
+console.log(Array.of(score7,score8,score9))
+
+
+
+
+
 
